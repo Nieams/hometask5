d18af33453f8722371e3380b19a8e1ceca887c8e
Nieams <raccoon2231gmail.com>
2023-04-04 00:40:23 +0300

    lab5

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import requests
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 import psycopg2
 
 app = Flask(__name__)
@@ -9,23 +9,36 @@ conn = psycopg2.connect(database="service_db",
           host="localhost",
           port="5432")
 cursor = conn.cursor()
-
-
+'''
 @app.route('/login/', methods=['GET'])
 def index():
     return render_template('login.html')
-
-@app.route('/login/', methods=['POST'])
+    '''
+@app.route('/login/', methods=['POST', 'GET'])
 def login():
-    username = request.form.get('username')
-    password = request.form.get('password')
-    if "" in [username, password]:
-        error_str = "Одно из полей пустое"
-        return render_template('error_page.html', error=error_str)
-    cursor.execute("SELECT * FROM service.users WHERE login=%s AND password=%s", (str(username), str(password)))
-    records = list(cursor.fetchall())
-    if not records:
-        error_str = "Пользователя нет в БД"
-        return render_template('error_page.html', error=error_str)
-    return render_template('account.html', full_name=records[0][1], username=records[0][2], password=records[0][3])
+    if request.method == 'POST':
+        if request.form.get("login"):
+            username = request.form.get('username')
+            password = request.form.get('password')
+            cursor.execute("SELECT * FROM service.users WHERE login=%s AND password=%s", (str(username), str(password)))
+            records = list(cursor.fetchall())
+            return render_template('account.html', full_name=records[0][1])
+        elif request.form.get("registration"):
+            return redirect("/registration/")
+    return render_template('login.html')
+
+@app.route('/registration/', methods=['POST', 'GET'])
+def registration():
+    if request.method == 'POST':
+        name = request.form.get('name')
+        login = request.form.get('login')
+        password = request.form.get('password')
+        cursor.execute('INSERT INTO service.users (full_name, login, password) VALUES (%s, %s, %s);', (str(name), str(login), str(password)))
+        conn.commit()
+
+        return redirect('/login/')
+
+        return render_template('registration.html')
+
+
 #http://localhost:5000/login/
